36d912e6b60325225b8d5b0ffc8f5cbe1d619ceb
Quan Ma <quantanma@gmail.com>
2019-01-30 22:54:09 -0500

    Intro + Property-Element Syntax

--- FirstApp/FirstApp/MainPage.xaml.cs
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace FirstApp
         {
             InitializeComponent();
         }
+
+        private void DemoButton_Clicked(object sender, EventArgs e)
+        {
+            DemoButton.Text = "I was clicked";
+        }
     }
 }
